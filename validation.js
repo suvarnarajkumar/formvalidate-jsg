db2b00fbb8f2eaeb2a2108fef60b1866d2866ee7
Suvarna <srajkumar_nyros@yahoo.com>
2014-08-27 11:58:20 +0530

    first commit

---
+++ validation.js
@@ -0,0 +1,187 @@
+
+/* ON SUBMIT CHECK ALL FIELDS ARE FILLED OR NOT*/
+function pageRedirect()
+{
+	if(document.getElementById('state').value === 100)
+	{
+   var USER_NAME=document.forms['registration']['userid'].value;	
+	var PASSWORD=document.forms['registration']['passid'].value;
+	var FIRST_NAME=document.forms['registration']['first_name'].value;
+	var LAST_NAME=document.forms['registration']['last_name'].value;
+	var FILE_NAME=document.registration.file_name.value;
+   var GENDER="null";
+   if (document.registration.sex[0].checked)
+   {
+  GENDER=document.registration.sex[0].value;
+	}
+   else
+   {
+   GENDER=document.registration.sex[1].value;
+   }
+ 	var D_O_B=document.forms['registration']['dob1'].value+"-"+document.forms['registration']['dob2'].value+"-"+document.forms['registration']['dob3'].value;
+   var HOBBIES_LIST="null";	
+   var i=0;
+	var list=0;
+	for(i=0;i<6;i++)
+	{	
+	if(document.registration.hob[i].checked)
+	{
+		if(list===0)
+		{
+		HOBBIES_LIST=	document.registration.hob[i].value;
+		list=1;
+		}	
+		else
+		{
+			HOBBIES_LIST=HOBBIES_LIST.concat(", "+document.registration.hob[i].value);
+		}
+	}
+	}
+	var COUNTRIES_LIST="null";
+	list=0;	
+	for (var i=1; i<document.registration.country.options.length; i++) 
+	{
+    if (document.registration.country.options[i].selected) 
+    {
+    		if(list===0)
+			{
+			 COUNTRIES_LIST=	document.registration.country.options[i].value;
+			list=1;
+			}
+			else
+			{	
+    		 COUNTRIES_LIST=COUNTRIES_LIST.concat(", "+document.registration.country.options[i].value);
+			}    
+    }
+	}
+
+	var PHONE_NUMBER=document.forms['registration']['phno'].value;
+			
+document.getElementById('status2').style.display='block';
+alert("Form Successfully Submitted with following information click 'ok'");
+alert("Account Information \n\n USERNAME\t:"+USER_NAME+"\n PASSWORD\t:"+ PASSWORD);
+alert("Profile Information \n\n FIRST NAME\t:"+FIRST_NAME+"\n LAST NAME\t:"+LAST_NAME+"\n FILE NAME\t:"+FILE_NAME+"\n GENDER\t\t:"+GENDER+"\n DATE OF BIRTH\t:"+D_O_B+"\n HOBBIES\t\t:"+HOBBIES_LIST+"\n HAVE BEEN TO\t:"+COUNTRIES_LIST);
+alert("Contact Information \n\n PHONE NUMBER\t:"+PHONE_NUMBER);
+if (confirm("Press 'OK' , if all the information is correct") == true) {return true;}
+else {return false;}
+}
+else if(document.getElementById('state').value === 0)
+{
+alert('Please fill all fields');
+document.getElementById("un_er").innerHTML='Please enter username';
+document.getElementById("pw_er").innerHTML='Please enter password';
+document.getElementById("cp_er").innerHTML='Please enter confirm password';
+document.getElementById("fn_er").innerHTML='Please enter first name';
+document.getElementById("ln_er").innerHTML='Please enter last name';
+document.getElementById("fi_er").innerHTML='Please upload file';
+document.getElementById("dt_er").innerHTML='Please select "day, month, year" in date of birth';
+document.getElementById("ho_er").innerHTML='Please select hobbies';
+document.getElementById("co_er").innerHTML='Please select country';
+document.getElementById("ph_er").innerHTML='Please enter phone number';
+document.forms['registration']['userid'].focus();
+return false;	
+}
+else
+{
+if(progress_bar0 === 0){document.getElementById("un_er").innerHTML='Please enter username';}
+if(progress_bar1 ===0){document.getElementById("pw_er").innerHTML='Please enter password';}
+if(progress_bar2 ===0){document.getElementById("cp_er").innerHTML='Please enter confirm password';}
+if(progress_bar3 ===0){document.getElementById("fn_er").innerHTML='Please enter first name';}
+if(progress_bar4 ===0){document.getElementById("ln_er").innerHTML='Please enter last name';}
+if(progress_bar5 ===0){document.getElementById("fi_er").innerHTML='Please upload file';}
+if(progress_bar6 ===0){document.getElementById("dt_er").innerHTML='Please select day in date of birth';}
+if(progress_bar7 ===0){document.getElementById("dt_er").innerHTML='Please select month in date of birth';}
+if(progress_bar8 ===0){document.getElementById("dt_er").innerHTML='Please select year in date of birth';}
+if(progress_bar10 ===0){document.getElementById("ho_er").innerHTML='Please select hobbies';}
+if(progress_bar11 ===0){document.getElementById("co_er").innerHTML='Please select country';}
+if(progress_bar9 ===0){document.getElementById("ph_er").innerHTML='Please enter phone number';}
+if(progress_bar0 === 0)
+{
+	document.forms['registration']['userid'].focus();
+}
+else
+{
+if(progress_bar1 ===0)
+{
+document.forms['registration']['passid'].focus();
+}
+else
+{
+if(progress_bar2 ===0)
+{
+document.forms['registration']['confirm_passid'].focus();
+}
+else
+{
+if(progress_bar3 ===0)
+{
+document.forms['registration']['first_name'].focus();
+}
+else
+{
+if(progress_bar4 ===0)
+{
+document.forms['registration']['last_name'].focus();
+}
+else
+{
+if(progress_bar5 ===0)
+{
+document.forms['registration']['file_name'].focus();
+}
+else
+{
+
+if(progress_bar6 ===0)
+{
+document.forms['registration']['dob1'].focus();
+}
+else
+{
+if(progress_bar7 ===0)
+{
+document.forms['registration']['dob2'].focus();
+}
+else
+{
+if(progress_bar8 ===0)
+{
+document.forms['registration']['dob3'].focus();
+}
+else
+{
+if(progress_bar10 ===0)
+{
+document.registration.hob[0].focus();
+}
+else
+{
+if(progress_bar11 ===0)
+{
+document.forms['registration']['country'].focus();
+}
+else
+{
+if(progress_bar9 ===0)
+{
+document.forms['registration']['phno'].focus();
+}
+}	
+}	
+}	
+}	
+}	
+}	
+}	
+}	
+}	
+}	
+}	
+
+return false;
+}
+
+}
+
+
+
---
+++ validations.js
@@ -0,0 +1,899 @@
+/* BACKGROUND COLOR EFFECT */
+function check(x)
+{
+document.bgColor=x;
+}
+
+/* PROGRESS_BAR VARIABLES FOR EACH FIELD VALIDATION */
+progress_bar13=0;
+progress_bar0=0;
+progress_bar1=0;
+progress_bar2=0;
+progress_bar3=0;
+progress_bar4=0;
+progress_bar5=0;
+progress_bar6=0;
+progress_bar7=0;
+progress_bar8=0;
+progress_bar9=0;
+progress_bar10=0;
+progress_bar11=0;
+progress_bar12=0;
+
+
+/* CHECKING FOR USERNAME VALID OR NOT  */
+function isAlphanumeric(obj,objto,min_len,max_len,str,objstar,objright,objwrong,obj_stat,obj_percent,obj_status,er_msg)
+{
+	
+if(obj.value.length === 0)
+{
+if(progress_bar0 === 1)
+	{
+		progress_bar0=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML="Please enter "+str;
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return;	
+}
+else if(obj.value.length >max_len ||obj.value.length <min_len)
+{
+	if(progress_bar0 === 1)
+	{
+		progress_bar0=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML=str+" should be in between "+min_len+" to "+max_len ;
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+	return;
+}
+else
+{
+	var letters = /^[a-zA-Z][a-zA-Z\d]+$/;
+if(!obj.value.match(letters))
+{
+	if(progress_bar0 === 1)
+	{
+		progress_bar0=0;
+		var val=obj_stat.value;
+		val=val-9;    obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML=str+" should be alphanumeric";
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return;
+}
+else
+{
+	er_msg.innerHTML="";
+	if(progress_bar0 === 0)
+	{
+	progress_bar0=1;
+	var val=obj_stat.value;
+		val=val+9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}
+objstar.style.color = "blue";
+objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+objto.focus();
+return ;
+}
+}
+}
+
+/* CHECKING PASSWORD FIELDS */
+function isPassword(obj,objto1,objto2,min_len,max_len,str,str1,objstar1,objstar2,objright1,objright2,objwrong1,objwrong2,obj_stat,obj_percent,obj_status,er_msg,er_msg1)
+{
+	if(obj.value.length === 0)
+{
+	if(progress_bar1 === 1)
+	{
+		progress_bar1=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML="Please enter "+str;
+objstar1.style.color = "red";
+objright1.style.visibility='hidden';
+objwrong1.style.visibility='visible';
+objto1.disabled = true;
+	
+
+if(objto1.value.length !==0 )
+{
+er_msg1.innerHTML=str1+" should not be different from above "+str;
+if(progress_bar2 === 1)
+		{
+		progress_bar2=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+		}	
+ objright2.style.visibility='hidden';
+objwrong2.style.visibility='visible';
+objstar2.style.color = "red";
+}
+
+obj.focus();
+return;
+}
+else if(obj.value.length >max_len ||obj.value.length <min_len)
+{
+	if(progress_bar1 === 1)
+	{
+		progress_bar1=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML=str+ " should be "+min_len+" to "+max_len;
+objstar1.style.color = "red";
+objright1.style.visibility='hidden';
+objwrong1.style.visibility='visible';
+objto1.disabled = true;
+	objright2.style.visibility='hidden';
+objwrong2.style.visibility='visible';
+objstar2.style.color = "red";
+
+if(objto1.value.length !==0 )
+{
+er_msg1.innerHTML=str1+" should not be different from above "+str;
+if(progress_bar2 === 1)
+		{
+		progress_bar2=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+		}	
+ objright2.style.visibility='hidden';
+objwrong2.style.visibility='visible';
+objstar2.style.color = "red";
+}
+
+obj.focus();
+return;
+}
+else
+{
+
+var letters = /^(?=.*\d.*)(?=.*[a-z].*)(?=.*[@#].*)(?=.*[A-Z].*)[a-zA-Z0-9@#]+$/;
+if(!obj.value.match(letters))
+{
+	if(progress_bar1 === 1)
+	{
+		progress_bar1=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML=str+"should be contain atleast one lowercase, uppercase, number, special character(@,#)";
+objstar1.style.color = "red";
+objright1.style.visibility='hidden';
+objwrong1.style.visibility='visible';
+objto1.disabled = true;
+er_msg1.innerHTML=str1+" should not be different from above "+str;
+	objright2.style.visibility='hidden';
+objwrong2.style.visibility='visible';
+objstar2.style.color = "red";
+obj.focus();
+return;
+}
+else
+{
+	er_msg.innerHTML="";
+	if(progress_bar1 === 0)
+	{
+	progress_bar1=1;
+	var val=obj_stat.value;
+		val=val+9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}
+	if(objto1.value.localeCompare(obj.value) ===0 )
+	{
+		if(progress_bar2 === 0)
+		{
+		progress_bar2=1;
+		var val=obj_stat.value;
+		val=val+9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+		}
+			objstar1.style.color = "blue";
+objright1.style.visibility='visible';
+objwrong1.style.visibility='hidden';
+er_msg1.innerHTML="";
+	objright2.style.visibility='visible';
+objwrong2.style.visibility='hidden';
+objstar2.style.color = "blue";
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+	objto1.disabled = false;
+	objto2.focus();
+		return
+	}
+	else
+	{
+		er_msg1.innerHTML=str1+" should not be different from above "+str;
+	objright2.style.visibility='hidden';
+objwrong2.style.visibility='visible';
+objstar2.style.color = "red";
+	objto1.disabled = false;
+objstar1.style.color = "blue";
+
+		objright1.style.visibility='visible';
+objwrong1.style.visibility='hidden';
+
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+objto1.focus();
+return ;
+}
+}
+}
+}
+
+
+
+
+
+
+/* CHECKING FOR CONFIRM PASSWORD VALID OR NOT */
+function isConfirmPassword(obj1,obj,objto,str,objstar,objright,objwrong,obj_stat,obj_percent,obj_status,er_msg)
+{
+	if(obj.value.length ===0)
+	{
+		if(progress_bar2 === 1)
+	{
+		progress_bar2=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML="Please enter "+str;
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return;
+}
+	else
+	{
+	if(obj.value.localeCompare(obj1.value) )
+{
+	if(progress_bar2 === 1)
+	{
+		progress_bar2=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML= str+ "should not be different from above password";
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return;
+}
+else
+{
+	er_msg.innerHTML="";
+	if(progress_bar2 === 0)
+	{
+		progress_bar2=1;
+		var val=obj_stat.value;
+		val=val+9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+objstar.style.color = "blue";
+		objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+objto.focus();
+return ;
+}
+}
+}
+
+/* CHECKING FOR FIRST NAME VALID OR NOT */
+function isAlphabetWithoutSpace(obj,objto,min_len,max_len,str,objstar,objright,objwrong,obj_stat,obj_percent,obj_status,er_msg)
+{
+	if(obj.value.length === 0)
+{
+	if(progress_bar3 === 1)
+	{
+		progress_bar3=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML="Please enter "+str;
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return;
+}
+else if(obj.value.length >max_len ||obj.value.length <min_len)
+{
+	if(progress_bar3 === 1)
+	{
+		progress_bar3=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML=str+" should be in between "+min_len+" to "+max_len;
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+	return;
+}
+else
+{	
+var letters = /^[a-zA-Z]+$/;
+if(!obj.value.match(letters))
+{
+	if(progress_bar3 === 1)
+	{
+		progress_bar3=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML=str+" should be Alphabets without spaces and dots.";
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return ;
+}
+else
+{
+	er_msg.innerHTML="";
+	if(progress_bar3 === 0)
+	{
+		progress_bar3=1;
+		var val=obj_stat.value;
+		val=val+9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+objstar.style.color = "blue";
+objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+objto.focus();
+return ;
+}
+}
+}
+
+/* CHECKING FOR LAST NAME VALID OR NOT */
+function isAlphabetWithSpace_Dot(obj,objto,min_len,max_len,str,objstar,objright,objwrong,obj_stat,obj_percent,obj_status,er_msg)
+{
+	if(obj.value.length === 0)
+{
+	if(progress_bar4 === 1)
+	{
+		progress_bar4=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg="Please enter "+str;
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return;
+}
+else if(obj.value.length >max_len ||obj.value.length <min_len)
+{
+	if(progress_bar4 === 1)
+	{
+		progress_bar4=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTM=str+" should be in between "+min_len+" to "+max_len ;
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+	return;
+}
+else
+{	
+	var letters = /^\S[a-zA-Z\. ]*\S$/;
+if(!obj.value.match(letters))
+{
+	if(progress_bar4 === 1)
+	{
+		progress_bar4=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML=str+" should be Alphabets with/without spaces and dots.\n But should not 'start and end' with spaces or dots";
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return ;
+}
+else
+{
+	er_msg.innerHTML="";
+	if(progress_bar4 === 0)
+	{
+		progress_bar4=1;
+		var val=obj_stat.value;
+		val=val+9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+objstar.style.color = "blue";
+objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+objto.focus();
+return;
+}
+}
+}
+
+
+
+/* CHECK THE FILE UPLOADED IS VALID FORMAT */
+function file_validation(obj,objto,str,formats,objstar,objright,objwrong,obj_stat,obj_percent,obj_status,er_msg)
+{	
+var f_name =obj.value;
+if(f_name.length === 0)
+{
+	if(progress_bar5 === 1)
+	{
+		progress_bar5=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML="Please upload a "+str;
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return;
+}
+else
+{
+var fileName = f_name.split(".");
+       fileName = fileName[fileName.length-1 ];
+var format= formats.split(",");
+var counts=0;
+for(var j=0;j<format.length;j++)
+{
+if(fileName.localeCompare(format[j]) ===0 )
+{
+	counts=1;
+	break;
+}
+}
+	
+if(counts!== 0)
+{	
+	er_msg.innerHTML="";
+	if(progress_bar5 === 0)
+	{
+		progress_bar5=1;
+		var val=obj_stat.value;
+		val=val+9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+
+	 objstar.style.color = "blue";
+objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+objto.focus();
+return;
+}
+else
+{
+	if(progress_bar5 === 1)
+	{
+		progress_bar5=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML=str +" should be of these formats "+formats;
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return;
+}
+
+}
+}
+
+/* CHECKING FOR GENDER VERIFICATION */
+function gender_validation(obj,objto,objstar,objright,objwrong,obj_stat,obj_percent,obj_status)
+{
+if(progress_bar13 === 0)
+	{
+		progress_bar13=1;
+		var val=obj_stat.value;
+		val=val+9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+	progress_bar13=1;
+	 objstar.style.color = "blue";
+objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+objto.focus();
+return;
+
+}
+
+
+/* CHECKING FOR DAY SELECTED OR NOT */
+function dob1select(obj,obj1,obj2,obj3,str,objstar,objright,objwrong,obj_stat,obj_percent,obj_status,er_msg)
+{
+if(obj.value == "default")
+{
+	if(progress_bar6 === 1)
+	{
+		progress_bar6=0;
+		var val=obj_stat.value;
+		val=val-3; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML='Select your '+str+' from the list';
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return;
+}
+else
+{
+	er_msg.innerHTML="";
+	if(progress_bar6 === 0)
+	{
+		progress_bar6=1;
+		var val=obj_stat.value;
+		val=val+3; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+	
+	
+if(obj1.value == "default")
+{
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj1.focus();
+return  ;
+}
+else
+{
+	
+if(obj2.value == "default")
+{
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj2.focus();
+return ;
+}
+else
+{
+ objstar.style.color = "blue";
+objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+obj3.focus();
+return;
+}
+}
+}
+}
+
+/* CHECKING FOR MONTH SELECTED OR NOT */
+function dob2select(obj,obj1,obj2,obj3,str,str1,str2,objstar,objright,objwrong,obj_stat,obj_percent,obj_status,er_msg)
+{
+if(obj.value == "default")
+{
+	if(progress_bar7 === 1)
+	{
+		progress_bar7=0;
+		var val=obj_stat.value;
+		val=val-3; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+
+er_msg.innerHTML='Select your '+str+' from the list';
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return;
+}
+else
+{
+	er_msg.innerHTML="";
+	if(progress_bar7 === 0)
+	{
+		progress_bar7=1;
+		var val=obj_stat.value;
+		val=val+3; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+
+if(obj1.value === "default")
+{
+er_msg.innerHTML='Select your '+str1+' from the list';
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj1.focus();
+return;
+}
+else
+{
+
+if(obj2.value === "default")
+{
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj2.focus();
+return;
+}
+else
+{
+
+objstar.style.color = "blue";
+objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+obj3.focus();
+return;
+}
+}
+}
+
+}
+
+
+/* CHECKING FOR YEAR SELECTED OR NOT */
+function dob3select(obj,obj1,obj2,obj3,str,str1,str2,objstar,objright,objwrong,obj_stat,obj_percent,obj_status,er_msg)
+{
+	
+if(obj.value == "default")
+{
+	if(progress_bar8 === 1)
+	{
+		progress_bar8=0;
+		var val=obj_stat.value;
+		val=val-4; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+
+	
+
+er_msg.innerHTML='Select your '+str+' from the list';
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return ;
+}
+else
+{
+	er_msg.innerHTML="";
+	if(progress_bar8 === 0)
+	{
+		progress_bar8=1;
+		var val=obj_stat.value;
+		val=val+4; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+	if(obj1.value === "default")
+{
+er_msg.innerHTML='Select your '+str1+' from the list';
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj1.focus();
+return ;
+}
+else
+{
+if(obj2.value === "default")
+{
+er_msg.innerHTML='Select your '+str2+' from the list';
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj2.focus();
+return ;
+}
+else
+{
+
+objstar.style.color = "blue";
+objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+obj3.focus();
+return ;
+}
+}
+}
+
+
+}
+
+/* CHECK PHONE NUMBER ENTERED OR NOT AND VALIDATE ENTERED PHONE NUMBER */
+function phone_validations(obj,objto,str,format,objstar,objright,objwrong,obj_stat,obj_percent,obj_status,er_msg)
+{
+	var res = obj.value.replace(/\d/g, 9 );
+if(res.localeCompare(format) !== 0)
+{
+	if(progress_bar9 === 1)
+	{
+		progress_bar9=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+	}	
+er_msg.innerHTML=str+" should be in this format "+format;
+objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+obj.focus();
+return;
+}
+else
+{
+	er_msg.innerHTML="";
+if(progress_bar9 === 0)
+	{
+		progress_bar9=1;
+		var val=obj_stat.value;
+		val=val+9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+		}
+	objstar.style.color = "blue";
+objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+objto.focus();
+return;
+}
+}
+
+
+
+/* CHECK HOBBIES SELECTED OR NOT */
+
+function checkbox_validation(obj,str,objstar,objright,objwrong,obj_stat,obj_percent,obj_status,er_msg)
+{
+var i=0;
+var list=0;
+for(i=0;i<6;i++)
+{
+	if(obj[i].checked)
+	{
+		list++;	
+	}
+}
+if(list === 0)
+{
+	if(progress_bar10 === 1)
+	{
+		progress_bar10=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+		}
+	er_msg.innerHTML="Select your "+str+" or atleast 'others'";
+	objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+
+	return;
+}
+	else
+	{
+		er_msg.innerHTML="";
+		if(progress_bar10 === 0)
+	{
+		progress_bar10=1;
+		var val=obj_stat.value;
+		val=val+9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+		}
+		objstar.style.color = "blue";
+objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+	return;		
+		}
+
+}
+
+/* CHECK COUNTRY SELECTED OR NOT */
+
+function multiple_select_validation(obj,str,objstar,objright,objwrong,obj_stat,obj_percent,obj_status,er_msg)
+{
+  var count = 0;
+	 for (var i=1; i<obj.options.length; i++) {
+    if (obj.options[i].selected) 
+    {
+    		 count++;
+    }
+ }
+    if(count === 0)
+{
+	if(progress_bar11 === 1)
+	{
+		progress_bar11=0;
+		var val=obj_stat.value;
+		val=val-9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+		}
+		
+	er_msg.innerHTML=str;
+	objstar.style.color = "red";
+objright.style.visibility='hidden';
+objwrong.style.visibility='visible';
+	return;
+}
+	else
+	{
+		er_msg.innerHTML="";
+		if(progress_bar11 === 0)
+	{
+		progress_bar11=1;
+		var val=obj_stat.value;
+		val=val+9; obj_percent.innerHTML=val+"%";
+		obj_stat.value=val;
+		}
+			objstar.style.color = "blue";
+objwrong.style.visibility='hidden';
+objright.style.visibility='visible';
+	if(obj_stat.value === 100)
+	{obj_status.style.display='block';}
+	return;		
+		}
+}
